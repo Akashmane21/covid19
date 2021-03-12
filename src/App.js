376e41e3475ea5372820d74148e6912e6820785a
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-03-12 15:12:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-Deploying is Happning        </p>
+change is Happning        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
